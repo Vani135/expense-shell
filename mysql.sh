5e065a531eae7b6932ccf9fc6f837c9041838bde
vani135 <vanipipallapali@gmail.com>
2024-04-29 13:28:44 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -41,7 +41,7 @@ VALIDATE $? "Starting MySQL Server"
 # VALIDATE $? "Setting up root password"
 
 #Below code will be useful for Idempotent nature
-mysql -h db.daws78s.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.20.229 -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then 
     mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
